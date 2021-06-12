cfaf62f9f3db52890a1033c692dac85d35629976
Zeta <zeta.hoffman@protonmail.com>
2021-06-12 16:34:13 -0600

    Version 3

--- SystemAdmin.UI/Controllers/HomeController.cs
+++ SystemAdmin.UI/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SystemAdmin.UI.Models;
 using SystemAdmin.UI.Models.ViewModels;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SystemAdmin.UI.Controllers
 {
@@ -15,26 +17,27 @@ namespace SystemAdmin.UI.Controllers
         public ActionResult Index()
         {
 
-            List<ListClientesViewModel> lst;
+            List<get_clientes_Result> lst;
             //  return View();
             using (ProyectoEntities db = new ProyectoEntities())
             {
-                lst = (from d in db.Cliente
-                       select new ListClientesViewModel
-                       {
-                           cedula = d.cedula,
-                           nombre = d.nombre,
-                           saldoGeneral = (float)d.saldoGeneral,
-                           direcconExacta = d.direccionExacta,
-                           idDistrito = d.idDistrito
-                       }).ToList();
+                //lst = (from d in db.Cliente
+                //       select new ListClientesViewModel
+                //       {
+                //           cedula = d.cedula,
+                //           nombre = d.nombre,
+                //           saldoGeneral = (float)d.saldoGeneral,
+                //           direcconExacta = d.direccionExacta,
+                //           idDistrito = d.idDistrito
+                //       }).ToList();
+                db.Configuration.LazyLoadingEnabled = false;
+                lst = db.get_clientes().ToList();
             }
-
+            
 
             return View(lst);
             
         }
-
 
 
       
